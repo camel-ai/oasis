cda8726442a46797204af74e436f87fe9316f947
YaoG722 <71317322+YaoG722@users.noreply.github.com>
2025-04-10 15:16:27 +0800

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -32,7 +32,7 @@ def check_port_open(host, port):
 
 
 if __name__ == "__main__":
-    host = "172.17.0.6"  # 平台 IP 地址
+    host = "0.0.0.0"  # 平台 IP 地址
     ports = [
         [8002, 8003, 8005],
         [8006, 8007, 8008],
@@ -43,31 +43,33 @@ if __name__ == "__main__":
         [8021, 8022, 8023],
         [8024, 8025, 8026],
     ]
-    gpus = [0]  # GPU 设备编号
+    gpus = [0, 1, 2]  # GPU 设备编号
 
-    all_ports = [port for i in gpus for port in ports[i]]
+    ports = [port for row in ports for port in row]  # 转换为一维数组
+
+    # all_ports = [port for i in gpus for port in ports[i]]
+    all_ports = ports[:len(gpus)]
     print("All ports: ", all_ports, '\n\n')
 
     t = None
-    for i in range(3):
-        for j, gpu in enumerate(gpus):
-            cmd = (
-                f"TF_ENABLE_DEPRECATION_WARNINGS=1 "
-                f"VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 "
-                f"VLLM_ENFORCE_CUDA_GRAPH=1 "
-                f"CUDA_VISIBLE_DEVICES=8 "  # 指定 GPU
-                f"python3 -m "
-                f"vllm.entrypoints.openai.api_server --model "
-                f"'/data/Qwen2.5-7B-Instruct' "  # 模型路径
-                f"--served-model-name 'Qwen2.5-7B' "  # 模型名称
-                # f"--tensor-parallel-size 1 "  # GPU 并行数
-                f"--host {host} --port {ports[j][i]} --gpu-memory-utilization "
-                f"0.9 --disable-log-stats")
-            t = threading.Thread(target=subprocess.run,
-                                 args=(cmd, ),
-                                 kwargs={"shell": True},
-                                 daemon=True)
-            t.start()
-        check_port_open(host, ports[0][i])
+    for i, gpu in enumerate(gpus):
+        cmd = (
+            f"TF_ENABLE_DEPRECATION_WARNINGS=1 "
+            f"VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 "
+            f"VLLM_ENFORCE_CUDA_GRAPH=1 "
+            f"CUDA_VISIBLE_DEVICES={gpu} "  # 指定 GPU
+            f"python3 -m "
+            f"vllm.entrypoints.openai.api_server --model "
+            f"'/data/Qwen2.5-7B-Instruct' "  # 模型路径
+            f"--served-model-name 'Qwen2.5-7B' "  # 模型名称
+            # f"--tensor-parallel-size 1 "  # GPU 并行数
+            f"--host {host} --port {ports[i]} --gpu-memory-utilization "
+            f"0.9 --disable-log-stats")
+        t = threading.Thread(target=subprocess.run,
+                                args=(cmd, ),
+                                kwargs={"shell": True},
+                                daemon=True)
+        t.start()
+        check_port_open(host, ports[i])
 
     t.join()
