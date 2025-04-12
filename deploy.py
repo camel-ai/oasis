4d5a2ca97ef1096e07d3673bcc8be4fb1b109e26
YaoG722 <71317322+YaoG722@users.noreply.github.com>
2025-04-12 14:42:14 +0800

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -46,6 +46,11 @@ if __name__ == "__main__":
     gpus = [0, 1, 2]  # GPU 设备编号
 
     ports = [port for row in ports for port in row]  # 转换为一维数组
+    '''
+    原脚本中会在一个GPU上部署3个API服务并对应3个端口，所以ports定义成了二维数组
+    因为我们平台的GPU显存不够大，需要改成一个GPU上部署一个API服务，对应一个端口
+    所以这里把ports数组按行转换为一维
+    '''
 
     # all_ports = [port for i in gpus for port in ports[i]]
     all_ports = ports[:len(gpus)]  # 输出将会使用的端口
@@ -54,17 +59,18 @@ if __name__ == "__main__":
     t = None
     for i, gpu in enumerate(gpus):
         cmd = (
-            f"TF_ENABLE_DEPRECATION_WARNINGS=1 "
-            f"VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 "
-            f"VLLM_ENFORCE_CUDA_GRAPH=1 "
+            f"TF_ENABLE_DEPRECATION_WARNINGS=1 "  # 启用TensorFlow的弃用功能警告
+            f"VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 "  # 允许模型支持超过默认最大长度的上下文
+            f"VLLM_ENFORCE_CUDA_GRAPH=1 "  # 强制使用CUDA Graph优化推理流程
             f"CUDA_VISIBLE_DEVICES={gpu} "  # 指定 GPU
             f"python3 -m "
-            f"vllm.entrypoints.openai.api_server --model "
-            f"'/models/Qwen2.5-7B-Instruct' "  # 模型路径
-            f"--served-model-name 'Qwen2.5-7B' "  # 模型名称
-            # f"--tensor-parallel-size 1 "  # GPU 并行数
-            f"--host {host} --port {ports[i]} --gpu-memory-utilization "
-            f"0.9 --disable-log-stats")
+            f"vllm.entrypoints.openai.api_server "
+            f"--model '/models/Qwen2.5-7B-Instruct' "  # 模型路径
+            f"--served-model-name 'Qwen2.5-7B' "  # 对外暴露的模型名称
+            # f"--tensor-parallel-size 8 "  # GPU 并行数，单卡部署需关闭
+            f"--host {host} --port {ports[i]} "  # 绑定IP和端口
+            f"--gpu-memory-utilization 0.9 "  # 设置GPU显存利用率阈值
+            f"--disable-log-stats")  # 关闭性能统计日志
         t = threading.Thread(target=subprocess.run,
                                 args=(cmd, ),
                                 kwargs={"shell": True},
